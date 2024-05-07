5cb6566f39660678ade48e0aff0bb9f36ee8479b
THT <thehelpfultipper@gmail.com>
2024-05-06 20:15:05 -0700

    Mobile responsive styles for product info

--- src/components/ProductPricing.jsx
+++ src/components/ProductPricing.jsx
@@ -4,8 +4,7 @@ import { getPrice } from "../helpers/utils";
 
 import s from "./Product.module.css";
 
-
-export default function ProductPricing({item}) {
+export default function ProductPricing({ item }) {
   const [isSale, setIsSale] = useState(false);
 
   const salePercent = `${Math.floor(item.sale * 100)}%`;
@@ -13,17 +12,17 @@ export default function ProductPricing({item}) {
   useEffect(() => {
     // False check on product pricing to determine sale
     // would occur via property check on product data
-    item?.sale !== '' && setIsSale(true);
+    item?.sale !== "" && setIsSale(true);
   }, []);
 
   return (
     <div className={s.priceWrapper}>
-      <span className={s.price}>{getPrice(item.currentPrice)}</span>
+      <div className={s.priceDiff}>
+        <span className={s.price}>{getPrice(item.currentPrice)}</span>
+        {isSale && <span className={s.discount}>{salePercent}</span>}
+      </div>
       {isSale && (
-        <>
-          <span className={s.discount}>{salePercent}</span>
-          <span className={s.fullPrice}>{getPrice(item.fullPrice)}</span>
-        </>
+        <span className={s.fullPrice}>{getPrice(item.fullPrice)}</span>
       )}
     </div>
   );
