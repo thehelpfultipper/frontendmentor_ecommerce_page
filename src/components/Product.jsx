0aaf2087a6cee0d8d4c124e5cfbf0438951c872a
THT <thehelpfultipper@gmail.com>
2024-05-08 08:33:02 -0700

    Adjust responsive styles for lightbox modal overlay

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,15 +5,17 @@ import ProductInfo from "./ProductInfo";
 import LightBox from "./LightBox";
 import useMyContext from "../hooks/useMyContext";
 import LightboxContext from "../context/lightbox-context";
+import useWindowSize from "../hooks/useWindowSize";
 
 import s from "./Product.module.css";
 
 export default function Product() {
   const { isLbShowing } = useMyContext(LightboxContext);
+  const {width} = useWindowSize();
 
   return (
     <>
-      {isLbShowing && <LightBox />}
+      {isLbShowing && width >= 1210 && <LightBox />}
       <ProductDisplay />
       <ProductInfo />
     </>
