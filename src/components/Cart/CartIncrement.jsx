c1a8dff9af25c1635b927938960bc5c4f8589254
THT <thehelpfultipper@gmail.com>
2024-05-22 08:47:59 -0700

    Update path of icon references

--- src/components/Cart/CartIncrement.jsx
+++ src/components/Cart/CartIncrement.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { getAssetPath } from '../../helpers/utils';
+
 import s from './Cart.module.css';
 
 export default function CartIncrement({inc, onInc, onIncAct}) {
@@ -14,9 +16,9 @@ export default function CartIncrement({inc, onInc, onIncAct}) {
   
   return (
     <div className={s.cartIncrementWrapper}>
-        <button className={`${s.incBtn} noBtn`} onClick={decHandler}><img src="/static/icon-minus.svg" alt="" /></button>
+        <button className={`${s.incBtn} noBtn`} onClick={decHandler}><img src={`${getAssetPath('static/icon-minus.svg')}`} alt="" /></button>
         <span className={s.cartQt}>{inc}</span>
-        <button className={`${s.incBtn} noBtn`} onClick={incHandler}><img src="/static/icon-plus.svg" alt="" /></button>
+        <button className={`${s.incBtn} noBtn`} onClick={incHandler}><img src={`${getAssetPath('static/icon-plus.svg')}`} alt="" /></button>
     </div>
   )
 }
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,3 +2,5 @@ export function getPrice(num) {
     const roundTwoDecimal = num.toFixed(2);
     return `$${roundTwoDecimal}`;
 }
+
+export const getAssetPath = (path) => `${import.meta.env.BASE_URL}${path}`;
