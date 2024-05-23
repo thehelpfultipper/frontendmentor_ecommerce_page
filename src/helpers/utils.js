84ee01434dae933666c48c0090ff9bc30e42b255
THT <thehelpfultipper@gmail.com>
2024-05-22 20:36:42 -0700

    Update base prefix for assets

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,6 +1,8 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 export function getPrice(num) {
     const roundTwoDecimal = num.toFixed(2);
     return `$${roundTwoDecimal}`;
 }
 
-export const getAssetPath = (path) => `${import.meta.env.BASE_URL}${path}`;
+export const getAssetPath = (path) => `${BASE_URL}${path}`;
